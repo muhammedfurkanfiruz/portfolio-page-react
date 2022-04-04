984f5021c4d2d52e9854fdd837a8c38f95e7a7a3
Furkan <mfurkan.firuz@gmail.com>
2022-04-04 17:44:29 +0300

    navbar created

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from "styled-components";
+import Navbar from './components/Navbar';
+// import Contact from "./components/Contact";
+// import Feature from "./components/Feature";
+// import Footer from "./components/Footer";
+// import Intro from "./components/Intro";
+// import Navbar from "./components/Navbar";
+// import Price from "./components/Price";
+// import Service from "./components/Service";
+
+const Container = styled.div`
+height:100vh;
+
+`
+
+
+const App = () => {
+  return (
+   <Navbar />
+  )
+}
+
+export default App
+
